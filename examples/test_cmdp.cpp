24db5deeae7d16ab9304d52c153721b899e549e2
basti_circa <sebastian.stickert@gmail.com>
2018-02-26 14:33:45 +0100

    integrated zlib and added cmakelist.txt to project

--- examples/test_cmdp.cpp
+++ examples/test_cmdp.cpp
@@ -2,9 +2,58 @@
 #include "../include/CMDParser.hpp"
 
 #include <zmq.hpp>
+#include <zlib.h>
+
 
 #include "../include/PointCloudGridEncoder.hpp"
 #include "../include/BinaryFile.hpp"
+
+unsigned long file_size(char *filename)
+{
+    FILE *pFile = fopen(filename, "rb");
+    fseek (pFile, 0, SEEK_END);
+    unsigned long size = ftell(pFile);
+    fclose (pFile);
+    return size;
+}
+
+int decompress_one_file(char *infilename, char *outfilename)
+{
+    gzFile infile = gzopen(infilename, "rb");
+    FILE *outfile = fopen(outfilename, "wb");
+    if (!infile || !outfile) return -1;
+
+    char buffer[128];
+    int num_read = 0;
+    while ((num_read = gzread(infile, buffer, sizeof(buffer))) > 0) {
+        fwrite(buffer, 1, num_read, outfile);
+    }
+
+    gzclose(infile);
+    fclose(outfile);
+}
+
+int compress_one_file(char *infilename, char *outfilename)
+{
+    FILE *infile = fopen(infilename, "rb");
+    gzFile outfile = gzopen(outfilename, "wb");
+    if (!infile || !outfile) return -1;
+
+    char inbuffer[128];
+    int num_read = 0;
+    unsigned long total_read = 0, total_wrote = 0;
+    while ((num_read = fread(inbuffer, 1, sizeof(inbuffer), infile)) > 0) {
+        total_read += num_read;
+        gzwrite(outfile, inbuffer, num_read);
+    }
+    fclose(infile);
+    gzclose(outfile);
+
+    printf("Read %ld bytes, Wrote %ld bytes, Compression factor %4.2f%%\n",
+       total_read, file_size(outfilename),
+       (1.0-file_size(outfilename)*1.0/total_read)*100.0
+    );
+}
 
 int main(int argc, char* argv[]){
     /*
@@ -30,7 +79,7 @@ int main(int argc, char* argv[]){
     encoder.settings.grid_precision = GridPrecisionDescriptor(
             Vec8(8,8,8), // dimensions
             bb,
-            Vec<BitCount>(BIT_4,BIT_4,BIT_4), // default point encoding
+            Vec<BitCount>(BIT_8,BIT_8,BIT_8), // default point encoding
             Vec<BitCount>(BIT_8,BIT_8,BIT_8)  // default color encoding
     );
 
